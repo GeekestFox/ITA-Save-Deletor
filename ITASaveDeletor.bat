acafff9a8806ced075af97897e57c95a6fd06e8c
GeekestFox <48397238+GeekestFox@users.noreply.github.com>
2019-10-29 18:45:11 -0400

    Update
    This will probably never be finshed

--- ITASaveDeletor.bat
+++ ITASaveDeletor.bat
@@ -3,3 +3,13 @@ echo Warning!!!!
 echo This software will permanitly delete ITA save data!!!!
 echo Only proceed if you know what your doing!!!!
 pause
+
+A:
+cls
+echo Pick a save to delete
+echo Save 1
+echo Save 2
+echo Save 3
+echo Save 4
+echo Save 5
+pause
